cc11a993f0192134b42195594a81a6e95c7193e3
yoshi161 <yosua161@gmail.com>
2016-01-27 20:30:12 -0800

    update to spark

--- src/main/java/com/hotel/yosi/Main.java
+++ src/main/java/com/hotel/yosi/Main.java
@@ -1,12 +1,22 @@
 package com.hotel.yosi;
 
 import static spark.Spark.get;
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class Main {
 	public static void main(String[] args) {
-
+		port(getHerokuAssignedPort());
 		staticFileLocation("/public");
         get("/hello", (req, res) -> "Hello World");
     }
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on
+						// localhost)
+	}
 }
